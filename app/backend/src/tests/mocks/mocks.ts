79e8521ecb0bffdacdcd2e2ef25c7352acf8572b
Renato <renatoft89@gmail.com>
2022-08-25 18:28:32 -0300

    feat: add mockTeam, mockMatche

--- app/backend/src/tests/mocks/mocks.ts
+++ app/backend/src/tests/mocks/mocks.ts
@@ -1,4 +1,6 @@
 import ILogin from '../../interfaces/ILogin'
+import IMatche from '../../interfaces/IMatche';
+import ITeam from '../../interfaces/ITeam';
 
 const mockUser: ILogin = {
   id: 1,
@@ -8,4 +10,52 @@ const mockUser: ILogin = {
   password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
 }
 
-export default mockUser;
+const mockTeam: ITeam[] = [
+  {
+    id: 16,
+    teamName: "São Paulo"
+  },
+  {
+    id: 1,
+    teamName: "Avaí/Kindermann"
+  },
+  {
+    id: 2,
+    teamName: "Bahia"
+  },
+]
+
+const mockMatche: IMatche[] = 
+  [
+    {
+      id: 1,
+      homeTeam: 16,
+      homeTeamGoals: 1,
+      awayTeam: 8,
+      awayTeamGoals: 1,
+      inProgress: false,
+      teamHome: {
+        teamName: "São Paulo"
+      },
+      teamAway: {
+        teamName: "Grêmio"
+      }
+    },
+    {
+      id: 41,
+      homeTeam: 16,
+      homeTeamGoals: 2,
+      awayTeam: 9,
+      awayTeamGoals: 0,
+      inProgress: true,
+      teamHome: {
+        teamName: "São Paulo"
+      },
+      teamAway: {
+        teamName: "Internacional"
+      }
+    }
+  ]
+
+
+export { mockUser, mockTeam, mockMatche };
